35454690e94153dbda11887b94441337ca6c4775
Denis Dudchenko <d-dud@yandex.ru>
2022-05-13 13:32:58 +0300

    :construction: Added date :construction: Added logotype

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@ const clock = document.getElementById('clock');
 
 setInterval(changeTime, 1000);
 
+const getNormalValue = val => val < 10 ? `0${val}` : val;
+
 function changeTime(){
     const date = new Date();
-    clock.dataset.hours = date.getHours();
-    clock.dataset.minutes = date.getMinutes();
-    clock.dataset.seconds = date.getSeconds();
+    clock.dataset.hours = getNormalValue(date.getHours());
+    clock.dataset.minutes = getNormalValue(date.getMinutes());
+    clock.dataset.seconds = getNormalValue(date.getSeconds());
 }
