1178fa9d0d0827596c3f617e202bbf367d1c1997
Kabanchegg1337 <d-dud@yandex.ru>
2022-05-23 21:16:53 +0300

    :construction_worker: Add webpack :construction: Added readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,76 @@
-const clock = document.getElementById('clock');
-const dateEl = document.getElementById('date');
+import "../css/style.css";
 
-const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+const getCssColor = (color) => `rgb(${color})`;
+window.wallpaperPropertyListener = {
+  applyUserProperties: function (properties) {
+    if (properties.color) {
+      const color = getCssColor(
+        properties.color.value.split(" ").map((c) => {
+          return Math.ceil(c * 255);
+        })
+      );
+      document.body.style.setProperty("--main-color", color);
+    }
+    if (properties.topgradientcolor) {
+      const topColor = getCssColor(
+        properties.topgradientcolor.value.split(" ").map((c) => {
+          return Math.ceil(c * 255);
+        })
+      );
+      document.body.style.setProperty("--gradient-top-color", topColor);
+    }
+    if (properties.bottomgradientcolor) {
+      const bottomColor = getCssColor(
+        properties.bottomgradientcolor.value.split(" ").map((c) => {
+          return Math.ceil(c * 255);
+        })
+      );
+      document.body.style.setProperty("--gradient-bottom-color", bottomColor);
+    }
+    if (properties.logotype) {
+      document.getElementById("logo").style.display = properties.logotype.value
+        ? "block"
+        : "none";
+    }
+    if (properties.perspective) {
+      const app = document.getElementById("app");
+      properties.perspective.value
+        ? app.classList.remove("no_perspective")
+        : app.classList.add("no_perspective");
+    }
+  },
+};
+
+const clock = document.getElementById("clock");
+const dateEl = document.getElementById("date");
+
+const monthNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
 ];
 
 setInterval(changeTime, 1000);
 
-const getNormalValue = val => val < 10 ? `0${val}` : val;
+const getNormalValue = (val) => (val < 10 ? `0${val}` : val);
 
-function changeTime(){
-    const date = new Date();
-    clock.dataset.hours = getNormalValue(date.getHours());
-    clock.dataset.minutes = getNormalValue(date.getMinutes());
-    clock.dataset.seconds = getNormalValue(date.getSeconds());
-    dateEl.innerText = `${monthNames[date.getMonth()]} ${getNormalValue(date.getDate())}-${getNormalValue(date.getMonth())}-${getNormalValue(date.getFullYear())}`
+function changeTime() {
+  const date = new Date();
+  clock.dataset.hours = getNormalValue(date.getHours());
+  clock.dataset.minutes = getNormalValue(date.getMinutes());
+  clock.dataset.seconds = getNormalValue(date.getSeconds());
+  dateEl.innerText = `${monthNames[date.getMonth()]} ${getNormalValue(
+    date.getDate()
+  )}-${getNormalValue(date.getMonth() + 1)}-${getNormalValue(
+    date.getFullYear()
+  )}`;
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Digital clock wallpaper
+
+## About project
+Digital clock wallpaper for wallpaper engine
+
+## Gallery
+<img src="resources\images\digital_clock.png?raw=true" />
+<img src="resources\images\digital_clock_2.png?raw=true" />
+<img src="resources\images\digital_clock_3.png?raw=true" />
+<img src="resources\images\digital_clock_4.png?raw=true" />
